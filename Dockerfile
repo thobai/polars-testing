1bc2e43211896ef5ce13a344b9ee8ab4e64f3ad1
Thomas Lutterbeck <thomas@tho-bai.de>
2022-07-13 17:09:31 +0200

    added testing with in-memory dataframe and Docker with polars from source

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,56 @@
-FROM python:3.10.4-slim-bullseye
+ARG polars_build_type=pip
+
+#
+# Polars from pip
+#
+FROM python:3.10.4-slim-bullseye as polars_pip
+RUN pip3 install polars
+
+#
+# Polars from source
+#
+FROM python:3.10.4-slim-bullseye as polars_source
+ENV polars_ver="source"
+
+ENV RUSTUP_HOME=/rust
+ENV CARGO_HOME=/cargo 
+ENV PATH=/cargo/bin:/rust/bin:$PATH
+
+RUN pip3 install maturin==0.12.18
+
+RUN apt-get update && \
+    DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
+        build-essential \
+        cmake \
+        ca-certificates \
+        curl \
+        git \
+        ssh \
+        libssl-dev \
+        pkg-config && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
+# RUN echo "(curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain nightly --no-modify-path) && source /cargo/env && rustup default nightly" > /install-rust.sh && \
+#    chmod 755 /install-rust.sh
+
+# RUN /install-rust.sh
+
+ADD docker-install-rust.sh /
+RUN chmod +x /docker-install-rust.sh ; \
+    /docker-install-rust.sh ; 
+
+# RUN git clone https://github.com/pola-rs/polars.git /polars
+
+# RUN echo "Installing polars from source..." && \
+#         cd /polars/py-polars && maturin build --rustc-extra-args="-C target-cpu=native" --release && \
+#         pip3 install /polars/py-polars/target/wheels/*manylinux*.whl ;
+# RUN rm -rf /polars
+
+#
+# General build
+#
+FROM polars_${polars_build_type}
 
 COPY polars_test.py /
 COPY requirements.txt /
--- polars_test.py
+++ polars_test.py
@@ -12,31 +12,62 @@ num_rows = 33000000
 
 df = pl.DataFrame(
     {
-        "pre": np.random.randint(15, size=num_rows, dtype=np.uint8),
-        "succ": np.random.randint(15, size=num_rows, dtype=np.uint8),
-        "duration": np.random.randint(10000, size=num_rows, dtype=np.uint16),
+        "pre": np.random.randint(15, size=num_rows, dtype=np.uint32),
+        "succ": np.random.randint(15, size=num_rows, dtype=np.uint32),
+        "duration": np.random.randint(10000, size=num_rows, dtype=np.uint32),
         "flag": np.random.binomial(2, 0.2, num_rows),
     }
 )
 
 df = df.with_columns([
-    # col("pre").cast(pl.Utf8).cast(pl.Categorical),
-    # col("succ").cast(pl.Utf8).cast(pl.Categorical),
     col("flag").cast(pl.Utf8).cast(pl.Categorical),
-    col("flag").cast(pl.Utf8).cast(pl.Categorical).alias("flag2"),
 ])
 
-df.write_parquet("random.parquet")
+df.write_parquet("random_int.parquet")
 
-df2 = pl.scan_parquet("random.parquet")
+df_cat = df.with_columns([
+    col("pre").cast(pl.Utf8).cast(pl.Categorical),
+    col("succ").cast(pl.Utf8).cast(pl.Categorical),
 
-with Timer("groupby"):
+])
+
+df_cat.write_parquet("random_cat.parquet")
+
+with Timer(text=f"groupby from in memory int\t{{milliseconds:.0f}}"):
     aggregated = (
-        df2
+        df
         .filter(col("flag") == lit("1"))
         .groupby(["pre", "succ"]).agg([
             col("duration").mean(),
             col("duration").median().alias("MedianDuration"),
             pl.count().alias("Count")
         ])
-    ).collect()
+    )
+
+
+with Timer(text=f"groupby from in memory cat\t{{milliseconds:.0f}}"):
+    aggregated = (
+        df_cat
+        .filter(col("flag") == lit("1"))
+        .groupby(["pre", "succ"]).agg([
+            col("duration").mean(),
+            col("duration").median().alias("MedianDuration"),
+            pl.count().alias("Count")
+        ])
+    )
+
+
+for f in ["random_int.parquet", "random_cat.parquet"]:
+
+    df2 = pl.scan_parquet(f)
+
+    with Timer(text=f"groupby {f}\t{{milliseconds:.0f}}"):
+        aggregated = (
+            df2
+            .filter(col("flag") == lit("1"))
+            .groupby(["pre", "succ"]).agg([
+                col("duration").mean(),
+                col("duration").median().alias("MedianDuration"),
+                pl.count().alias("Count")
+            ])
+        ).collect()
